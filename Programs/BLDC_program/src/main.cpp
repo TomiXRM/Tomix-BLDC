1687b6bfafc9f6ed02612c7b7a316c4d81ca0ec9
Daiki Tomioka <chocolate0785lego@icloud.com>
2022-02-02 21:51:06 +0900

    角度情報を取り出せた

--- Programs/BLDC_program/src/main.cpp
+++ Programs/BLDC_program/src/main.cpp
@@ -7,23 +7,14 @@ InterruptIn Hall2(D12);
 RawSerial pc(USBTX,USBRX,1000000);
 bool hallState[3];
 
-int16_t state;
-uint8_t hallStateAsBit;
-
-
-void printbits(uint8_t v) {
-      for(int8_t i = 7; i >= 0; i--) putchar('0' + ((v >> i) & 1));
-      putchar('\n');
-}
-
+uint8_t state,statePrev,hallStateAsBit;;
+int16_t count,degree;
 
 void hallHandle(){
       hallState[0] = Hall1.read();
       hallState[1] = Hall2.read();
       hallState[2] = Hall3.read();
       hallStateAsBit = Hall1.read() << 1 | Hall2.read() | Hall3.read() << 2;
-      // printbits(hallStateAsBit);
-      // pc.printf(" %d\r\n",hallStateAsBit);
       switch (hallStateAsBit) {
       case 1: // 001
             state = 1; break;
@@ -40,31 +31,15 @@ void hallHandle(){
       default:
             break;
       }
-      pc.printf(" %d\r\n",state);
+      if(statePrev != state && ((statePrev - state) == 1 || (statePrev - state) == -5)) {
+            count++;
+      }else if(statePrev != state && ((statePrev - state) == -1 || (statePrev - state) == 5)) {
+            count--;
+      }
+      degree = (count*75)%3600;
+      statePrev = state;
+      pc.printf(" %d count:%d\t degree:%d\r\n",state,count,degree);
 }
-
-// static unsigned int getHallPosition(void){
-//       if( (HALL1==HALL_HIGH) && (HALL2==HALL_LOW) && (HALL3==HALL_HIGH) ) {
-//             return 1;
-//       }
-//       else if( (HALL1==HALL_HIGH) && (HALL2==HALL_LOW) && (HALL3==HALL_LOW) ) {
-//             return 2;
-//       }
-//       else if( (HALL1==HALL_HIGH) && (HALL2==HALL_HIGH) && (HALL3==HALL_LOW) ) {
-//             return 3;
-//       }
-//       else if( (HALL1==HALL_LOW) && (HALL2==HALL_HIGH) && (HALL3==HALL_LOW) ) {
-//             return 4;
-//       }
-//       else if( (HALL1==HALL_LOW) && (HALL2==HALL_HIGH) && (HALL3==HALL_HIGH) ) {
-//             return 5;
-//       }
-//       else if( (HALL1==HALL_LOW) && (HALL2==HALL_LOW) && (HALL3==HALL_HIGH) ) {
-//             return 6;
-//       }else{
-//             return 0;
-//       }
-// }
 
 int  main(){
       Hall3.rise(&hallHandle);
@@ -73,9 +48,12 @@ int  main(){
       Hall3.fall(&hallHandle);
       Hall1.fall(&hallHandle);
       Hall2.fall(&hallHandle);
+
+      count = 0;
       while(1) {
 
             // pc.printf("count:%d\r\n",count);
-            wait_ms(10);
+            // wait_ms(10);
+
       }
 }
