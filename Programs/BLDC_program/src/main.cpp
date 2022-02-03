bc8b7ac0a42ae131e19229b22c1c4f443420d3ae
Tomix <chocolate0785lego@icloud.com>
2022-02-03 23:42:37 +0900

    三角関数テーブルを追加

--- Programs/BLDC_program/src/main.cpp
+++ Programs/BLDC_program/src/main.cpp
@@ -1,5 +1,8 @@
 #include <mbed.h>
 // Hall  3 1 2
+#include "FastPWM.h"
+#include "TrigonometricTable.h"
+
 InterruptIn Hall3(D10);
 InterruptIn Hall1(D11);
 InterruptIn Hall2(D12);
@@ -8,7 +11,12 @@ RawSerial pc(USBTX,USBRX,1000000);
 bool hallState[3];
 
 uint8_t state,statePrev,hallStateAsBit;;
-int16_t count,degree;
+int16_t count,degree,theta;
+
+// D3(PB_0) D6(PB_1) A2(PA3)
+PwmOut pwmU(D3);
+PwmOut pwmV(D6);
+PwmOut pwmW(A2);
 
 void hallHandle(){
       hallState[0] = Hall1.read();
@@ -50,10 +58,17 @@ int  main(){
       Hall2.fall(&hallHandle);
 
       count = 0;
+      theta = 0;
+
+      pwmU.period_us(10);
+      pwmV.period_us(10);
+      pwmW.period_us(10);
+
       while(1) {
-
-            // pc.printf("count:%d\r\n",count);
-            // wait_ms(10);
-
+            pwmU.write(0.5*sin32_T(theta)+0.5);
+            pwmV.write(0.5*sin32_T(theta+120)+0.5);
+            pwmW.write(0.5*sin32_T(theta-120)+0.5);
+            theta ++;
+            // wait_ms(1);
       }
 }
