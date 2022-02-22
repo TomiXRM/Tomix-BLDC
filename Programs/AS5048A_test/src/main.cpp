3378ee5fd740e80af12130a7ac6e984365152c62
Tomix <chocolate0785lego@icloud.com>
2022-02-23 02:27:16 +0900

    自作三角関数テーブルアップデート

--- Programs/AS5048A_test/src/main.cpp
+++ Programs/AS5048A_test/src/main.cpp
@@ -1,35 +1,31 @@
 #include "mbed.h"
 #include <AS5048A.h>
-
-// The sensors connection are attached to pins 5-8
+#include "TrigonometricTable.h"
 As5048Spi sensor(D11, D12, D13, D10);
 RawSerial pc(USBTX, USBRX,1000000); // tx, rx
 Timer timer;
 int timeT;
-
+int s,c;
+int i = 0;
 int main() {
       timer.start();
+      sensor.frequency(8000000);
       while(1) {
+            // timer.reset();
+            // int angle = *sensor.read_angle();
+            // if(sensor.parity_check(angle)) {
+            //       int degrees = As5048Spi::degrees(angle)/100;
+            //       timeT = timer.read_us();
+            //       pc.printf("%i,%d\r\n",degrees,timeT);
+            // }else{
+            //       pc.printf("Parity check failed.\r\n");
+            // }
+            // wait_ms(1);
+            i++;
             timer.reset();
-            const int* angles = sensor.read_angle();
-            int angle = angles[0];
-
-            // The read angle returns the value returned over the SPI bus, including parity bit
-            // pc.printf("Read result: %x\r\n", angle);
-
-            if( As5048Spi::parity_check(angle) )
-            {
-                  // Convert range from 0 to 2^14-1 to 0 - 360 degrees
-                  int degrees = As5048Spi::degrees(angle)/100;
-                  timeT = timer.read_us();
-                  // pc.printf("Parity check succesfull.\r\n");
-                  pc.printf("%i,%d\r\n",degrees,timeT);
-            }
-            else
-            {
-                  pc.printf("Parity check failed.\r\n");
-            }
-
-            wait_ms(1); 
+            s = (int)(sin32_T(i)*1000);
+            c = (int)(cos32_T(i)*1000);
+            timeT = timer.read_us();
+            pc.printf("%d,%d,%d\r\n",s,c,timeT);
       }
 }
